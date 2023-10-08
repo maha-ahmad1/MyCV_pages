2ea51c308e8192c84c6014ab39bfe471b6924cd0
maha-ahmad1 <maha.ahmad.k@hotmail.com>
2023-10-08 22:44:07 +0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="h-screen">
+      <main>
+      <div className="bg-black py-4  text-white">maha</div>
+      </main>
+    </div>
+  );
+}
